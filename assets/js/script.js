a6795185ea2f6a7cb6d8166a76eb48080df1b761
ferdinandmukodo <ferdinant.mukodo@student.moringaschool.com>
2022-07-20 09:12:01 +0300

    It adds js code

---
+++ assets/js/script.js
@@ -0,0 +1,10 @@
+function calculateIncome(){
+   
+    let
+
+  document.getElementsById(shed_A).innerHTML = "Your production in shed_A is" +
+  document.getElementById(shed_B).innerHTML = "Your production in shed_B is" +
+  document.getElementById(shed_C).innerHTML = "Your production in shed_c is" +
+  document.getElementById(shed_D).innerHTML = " Your production in shed_D is" +
+}
+
